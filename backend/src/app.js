d0bb225032a9036feeb6e5d170ab5a9dad1546a7
MarisolAL16 <marialfarolo@est.utn.ac.cr>
2026-06-16 09:37:41 -0600

    Vers:2 Se implementa archivo .env
    Se cargan las variables en app.js y se ejecutan en el puerto 4000

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,8 +1,16 @@
+import dotenv from "dotenv/config"; //Si la ruta del .env en otro .env, "dotenv/config(.env.development)"
 import express from "express";
+const { NAME, VERSION, DESCRIPTION, AUTHOR } = process.env; // Obtener las variables de entorno del archivo .env
 
 const app = express(); // Crear una instancia de Express
 
 app.get("/", (req, res) => { // Crear una ruta para la raíz del servidor
+   res.json({
+        NAME: NAME,
+        VERSION: VERSION,
+        DESCRIPTION: DESCRIPTION,
+        AUTHOR: AUTHOR,
+   });
     res.send('<h1>Hola JP</h1>');
 });
 
