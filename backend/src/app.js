023353567d80563bc698e0fb39410ea4eb567679
MarisolAL16 <marialfarolo@est.utn.ac.cr>
2026-06-16 10:08:02 -0600

    Se implementa CORS para acceder a la API
    Permite solicitudes fuera de backend

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,8 +1,12 @@
 import dotenv from "dotenv/config"; //Si la ruta del .env en otro .env, "dotenv/config(.env.development)"
 import express from "express";
+import cors from "cors";
 const { NAME, VERSION, DESCRIPTION, AUTHOR } = process.env; // Obtener las variables de entorno del archivo .env
 
 const app = express(); // Crear una instancia de Express
+
+app.use(cors()); // Habilitar CORS para permitir solicitudes desde el frontend
+app.use(express.json()); // Habilitar el análisis de JSON en las solicitudes entrantes
 
 app.get("/", (req, res) => { // Crear una ruta para la raíz del servidor
    res.json({
@@ -11,7 +15,6 @@ app.get("/", (req, res) => { // Crear una ruta para la raíz del servidor
         DESCRIPTION: DESCRIPTION,
         AUTHOR: AUTHOR,
    });
-    res.send('<h1>Hola JP</h1>');
 });
 
 app.listen(4000, () => { // Iniciar el servidor en el puerto 4000
